cb10b05a5f464c45c7aee32554f516dc1f53f126
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-08-20 17:57:46 -0400

    fix: Fix issue with stow in path

--- makefiles/stow.mk
+++ makefiles/stow.mk
@@ -1,15 +1,22 @@
 # homebrew - GNU Stow
 
 OS := $(shell uname -s)
+ARCH := $(shell uname -p)
+
+ifeq ($(OS),Darwin)
+ifeq ($(ARCH),i386)
+	STOW_BIN := /usr/local/bin/stow
+endif
+ifeq ($(ARCH),arm)
+	STOW_BIN := /opt/homebrew/bin/stow
+endif
+endif
+
 
 .PHONY: link
 link: ## Setup symlinks
-ifeq (, $(shell which stow))
-	$(error "No stow in $$PATH")
-endif
-
 	# Shared
-	stow stow
+	$(STOW_BIN) stow
 
 ifeq ($(OS),Darwin)
 	# TODO
