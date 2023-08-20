00285f0b95634ac5928b4960d9bb2c3a97ad880b
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-08-20 16:34:48 -0400

    feat: macOS: homebrew: core

--- makefiles/macos_pkgs.mk
+++ makefiles/macos_pkgs.mk
@@ -1,6 +1,12 @@
 # Makefile - MacOS Packages (Homebrew)
 
-BREW_BIN := /usr/local/bin/brew
+ARCH := $(shell uname -p)
+ifeq ($(ARCH),i386)
+	BREW_BIN := /usr/local/bin/brew
+endif
+ifeq ($(ARCH),arm)
+	BREW_BIN := /opt/homebrew/bin/brew
+endif
 
 .PHONY: pkgs_all
 pkgs_all: install_brew ## Install all pkgs
@@ -9,3 +15,6 @@ pkgs_all: install_brew ## Install all pkgs
 install_brew:
 	/bin/bash -c "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+.PHONY: pkgs_core
+pkgs_core: ## Install core packages
+	$(BREW_BIN) bundle --file=homebrew/Brewfile-core
