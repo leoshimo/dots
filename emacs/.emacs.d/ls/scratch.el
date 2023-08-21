b6fe241a195d43e21ec008919667ab6ffa9b7d92
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-08-20 21:46:33 -0400

    chore: Remove persistent-scratch

--- emacs/.emacs.d/ls/scratch.el
+++
@@ -1,9 +0,0 @@
-;;
-;; scratch.el - scratch buffer
-;;
-
-;; persistent-scratch - https://github.com/Fanael/persistent-scratch
-(use-package persistent-scratch
-  :config
-  (persistent-scratch-setup-default))
-    
