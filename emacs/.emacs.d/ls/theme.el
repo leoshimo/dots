9afc144bd9dec0c161040e44123921c9e8173ad3
leoshimo <56844000+leoshimo@users.noreply.github.com>
2024-05-24 09:54:40 -0700

    feat(emacs): ACME

--- emacs/.emacs.d/ls/theme.el
+++ emacs/.emacs.d/ls/theme.el
@@ -11,13 +11,16 @@
 ;; modus-themes - https://protesilaos.com/emacs/modus-themes
 (use-package modus-themes
   :config
-  (load-theme 'modus-vivendi-tritanopia t)
-  (setq modus-themes-to-toggle '(modus-vivendi-tritanopia modus-operandi-tinted))
-)
+  (setq modus-themes-to-toggle '(modus-vivendi-tritanopia modus-operandi-tinted)))
 
 ;; smart-mode-line
+
 (use-package smart-mode-line
   :config
   (setq sml/no-confirm-load-theme t
         sml/theme 'respectful)
   (sml/setup))
+
+(use-package acme-theme
+  :config
+  (load-theme 'acme t))
